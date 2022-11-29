881e09203e9bc5ef2a3fd73348240db1e83f7bf9
Alejandro Velasquez <velasale@oregonstate.edu>
2022-11-28 16:15:20 -0800

    Adding random learning as baseline

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ large_row32 = [0, 0, -20, -10, -10, -20, 0, 0, -20, -10, -10, -20, 0,
                -20, -10, -10, -20, 0, 0]
 small_row8 = [0, 0, -20, -10, -10, -20, 0, 0]
 
-
 # action flow ( ex: 3 -> 2 -> -10(done) )
 default_action_sequence = {1: -10, 2: -10, 3: 2, 4: 1, -10: -10}
 # Action mappings, A pruner with action type 1 can do action sequences 1 or 3
@@ -20,17 +19,18 @@ default_prob = [.2, .1, .3, .3, .1]
 # Tree representations (Mostly for GUI)
 default_tree_combos = [1, 2, 3, 4, -10]
 
-
 # Alejo's To check with only 1 agent and one tree
 default_prob = [0.5, 0.5, 0]
 default_tree_combos = [1, 2, -10]
 
 # Design A:
-small_row8 = [0, 0, 0, -20, -10, -20, 0, 0, 0]
+# small_row8 = [0, 0, 0, -20, -10, -20, 0, 0, 0]
 
 # Design A:
 # tstep = 500, episodes = 5000, epsilon = 0.99
-# small_row8 = [0, 0, 0, -20, -10, -20, 0, 0, 0, -20, -10, -20, 0, 0, 0]
+small_row8 = [0, 0, 0, -20, -10, -20, 0, 0, 0, -20, -10, -20, 0, 0, 0]
+
+
 # small_row8 = [0, 0, 0, -20, -10, -20, 0, 0, -20, -10, -10, -20, 0, 0]
 
 
@@ -54,27 +54,21 @@ def large_orchard():
     # test.run()
 
 
-def small_orchard():
+def small_orchard(approach: str):
     # 2 agents even split between pick and prune
     # 8x13
 
     # Grid parameters
-    top_buffer = 2
-    bottom_buffer = 2
+    top_buffer = 3
+    bottom_buffer = 3
     row_height = 2
 
     # Algorithm parameters
-    time_steps = 100
-    episodes = 500
+    time_steps = 500
+    episodes = 1000
 
     # Create Orchard
     small_orchard = orchard.OrchardMap(
-        row_height=row_height, row_description=small_row8, top_buffer=top_buffer, bottom_buffer=bottom_buffer,
-        action_sequence=default_action_sequence, action_map=default_action_map, tree_prob=default_prob,
-        tree_combos=default_tree_combos)
-
-    # Create Orchard
-    small_orchard_r = orchard.OrchardMap(
         row_height=row_height, row_description=small_row8, top_buffer=top_buffer, bottom_buffer=bottom_buffer,
         action_sequence=default_action_sequence, action_map=default_action_map, tree_prob=default_prob,
         tree_combos=default_tree_combos)
@@ -83,39 +77,31 @@ def small_orchard():
     rows = row_height + top_buffer + bottom_buffer
     cols = len(small_row8)
     agent_list = []
-    agent_list_r = []
+
     for i in range(1):
         a = orchard_agents.AgentPick(rows, cols)
         agent_list.append(a)
-        a_r = orchard_agents.AgentPick(rows, cols)
-        agent_list_r.append(a_r)
     for i in range(1):
         a = orchard_agents.AgentPrune(rows, cols)
         agent_list.append(a)
-        a_r = orchard_agents.AgentPrune(rows, cols)
-        agent_list_r.append(a_r)
-
-    # Learn
-    test_r = orchard.OrchardSim(orchard_map=small_orchard_r, agents=agent_list_r, tstep_max=time_steps, ep_max=episodes)
-    # test.run_gui()
-    # To run without GUI (Way faster)
-    test_r.run("random")
 
     test = orchard.OrchardSim(orchard_map=small_orchard, agents=agent_list, tstep_max=time_steps, ep_max=episodes)
     # test.run_gui()
     # To run without GUI (Way faster)
-    test.run("local")
+    test.run(approach)
 
-    # Step3: Plot Results
-    for i in range(len(test_r.agents)):
-        # tl.plot_reward(test_r.agents[i].reward_evolution, i)
-        tl.plot_reward_and_baseline(test.agents[i].reward_evolution, test_r.agents[i].reward_evolution, i)
-        tl.plot_values(test_r.agents[i].q_sa_table, i)
-    plt.show()
+    return test
 
 
 if __name__ == "__main__":
-    test = small_orchard()
+
+    test = small_orchard("local")
+    # test_random = small_orchard("random")
+
+    for i in range(len(test.agents)):
+        tl.plot_reward(test.agents[i].reward_evolution, i)
+        # tl.plot_reward_and_baseline(test.agents[i].reward_evolution, test_random.agents[i].reward_evolution, i)
+        tl.plot_values(test.agents[i].q_sa_table, i)
+    plt.show()
+
     # test = large_orchard()
-
-
